a237ab3d39e6ea0835e20f91932641c4525ef18c
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2024-01-29 18:56:34 +0530

    Included a favicon Updated project section and fixed minor changes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Scroll To Top Functionality
-
 const toTop = document.querySelector('.to-top');
 // console.log(toTop)
 window.addEventListener('scroll', () => {
@@ -18,24 +17,29 @@ toTop.addEventListener('click', (e) => {
 })
 
 
-
 // Responsive Navbar Functionality
-
 let respHead = document.querySelector('.resp-head');
 let toggleBtn = document.querySelector('.toggleBtn');
 let respNav = document.querySelector('.resp-nav');
+let faSolid = document.querySelector('.fa-solid');
 
 // console.log(respHead, toggleBtn, respNav)
-
 toggleBtn.addEventListener('click', (e) => {
     e.preventDefault()
     if (respNav.style.display != 'flex') {
         respHead.style.height = '20rem';
         respNav.style.display = 'flex'
+        faSolid.classList.add('fa-xmark');
     }
     else {
+        faSolid.classList.remove('fa-xmark');
         respHead.style.height = '9rem';
         respNav.style.display = 'none'
     }
 })
 
+
+// Get Current Year
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById('getYear').innerHTML = year;
